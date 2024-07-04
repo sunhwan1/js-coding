5069b6853c88786b4300aafcb2b5bf31122c3c16
kwonsunhwan <240204@ict.ac.kr>
2024-07-04 17:44:14 +0900

    한국기사 추가

--- newsserver/newsapi.js
+++ newsserver/newsapi.js
@@ -4,13 +4,16 @@ const axios = require('axios').default;
 
 const API_KEY = 'fc1037af1e2d45e28f055d296531586e';
 
-const getApiData = async () => {
+// Fetch data from News API
+const getApiData = async (country, category = 'business') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}`;
     try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${API_KEY}`);
+        const response = await axios.get(url);
         return response.data.articles.map(article => ({
             title: article.title,
             author: article.author,
             publishedAt: article.publishedAt,
+            url: article.url,
             imageUrl: article.urlToImage
         }));
     } catch (error) {
@@ -22,19 +25,26 @@ const getApiData = async () => {
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', async (req, res) => {
+// Render news page
+const renderNewsPage = async (res, country, viewName) => {
     try {
-        const data = await getApiData();
-        res.render('index', {
-            one: "this is one", // 이 부분을 확인하고 있음
-            two: "this is two", // 이 부분을 확인하고 있음
+        const data = await getApiData(country);
+        res.render(viewName, {
+            one: "this is one",
+            two: "this is two",
             data: data
         });
     } catch (error) {
-        console.error('Error rendering index:', error);
+        console.error(`Error rendering ${viewName}:`, error);
         res.status(500).send('Internal Server Error');
     }
-});
+};
+
+// US news route
+app.get('/', (req, res) => renderNewsPage(res, 'us', 'index'));
+
+// Korean news route
+app.get('/domestic', (req, res) => renderNewsPage(res, 'kr', 'domestic'));
 
 const port = 8000;
 app.listen(port, () => {
